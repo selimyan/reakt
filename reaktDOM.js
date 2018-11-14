bcb7855ee57ce129832000e0895fb051cad768c6
selimyan <gselimyan@gmail.com>
2018-11-14 11:16:14 -0500

    handleProps and handleChildren

--- reaktDOM.js
+++ reaktDOM.js
@@ -47,38 +47,39 @@ function renderNode(vNode) {
 
     return element
   }
+}
 
-  //handle children
-  children.forEach(child => {
+function handleChildren(children, element) {
+  (children || []).forEach(child => {
     if (typeof child === 'string') {
       element.appendChild(document.createTextNode(child))
-    } else {
-      element.appendChild(renderNode(child));
+    }
+    else {
+      element.appendChild(renderNode(child))
     }
   })
+}
 
-  //handle props
+function handleProps(props, element) {
   for (let prop in props) {
 
+    // events
     if (/^on/.test(prop)) {
-      const eventName = prop.slice(2).toLowerCase()
+      const eventName = prop.substring(2).toLowerCase()
       element.addEventListener(eventName, props[prop])
       console.log('event')
     }
 
+    // DOM properties
     else if (prop in element) {
       element[prop] = props[prop]
     }
 
+    // custom attributes
     else {
       element.setAttribute(prop, props[prop])
     }
   }
-
-  element.appendChild(document.createTextNode(children[0]))
-
-  return element
-
 }
 
 export function render(vNode, node) {
